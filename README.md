c5b3a6f5fe5440ef3d7a5c4592f830febae22ea1
Aslan <aslan4ig99@gmail.com>
2021-09-29 12:21:14 +0700

    v1.0

---
+++ README.md
@@ -0,0 +1,13 @@
+# IOS App
+- Only Horizontal position
+- UIKit
+- MVC
+## How to play:
+- slider have value from 1 to 50
+- tap "Проверить"
+- after tap label generate random number from 1 to 50
+- U need move slider at number that equal generated number
+- U have 5 rounds
+- after 5 rounds present you alert with your result point 
+
+![Screenshot](screenshot.png)
---
+++ Right on target/Right on target/ViewController.swift
@@ -0,0 +1,44 @@
+//  ViewController.swift
+//  Right on target
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var label: UILabel!
+    var number: Int = 0
+    var round: Int = 0
+    var points: Int = 0
+    @IBAction func checkNumber() {
+        if self.round == 0 {
+            self.number = Int.random(in: 1...50)
+            self.label.text = String(self.number)
+            self.round += 1
+        }
+        else {
+            let numSlider = Int(self.slider.value.rounded())
+            if numSlider > number {
+                points += 50 - numSlider + number
+            }
+            else if numSlider < number {
+                points += 50 - number + numSlider
+            }
+            else {
+                points += 50
+            }
+            if self.round == 5 {
+                let alert = UIAlertController.init(
+                    title: "Игра окончена",
+                    message: "Вы заработали \(points) очков",
+                    preferredStyle: .alert)
+                alert.addAction(UIAlertAction.init(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                round += 1
+            }
+            self.number = Int.random(in: 1...50)
+            self.label.text = String(self.number)
+        }
+    }
+}
+
